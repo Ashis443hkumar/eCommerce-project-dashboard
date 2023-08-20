e61d0c54986e74fee2fa06ee17f7d3812a97eb89
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-08-20 16:45:05 +0530

    1 file modified

--- src/components/pages/home/home3/Furniture.jsx
+++ src/components/pages/home/home3/Furniture.jsx
@@ -1,13 +1,15 @@
 
 import React from "react";
 import {Container, Row,Col } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 
 export default function Furniture() {
   return (
    <>
      <section className="px-5 py-5 my-5">
-     <Container fluid>
+      <Container fluid>
        <Row>
          <Col lg={3} className="bg-white p-4">
          <div className="left_menu4">
@@ -47,7 +49,7 @@ export default function Furniture() {
          </Row>
          <Row className="mt-4">
            <Col lg={8}>
-             <img src="https://bazaar.ui-lib.com/assets/images/Furniture%20Shop/Furniture%20(6).png" className="w-100 h-25" alt=""/>
+             <img src="https://bazaar.ui-lib.com/assets/images/Furniture%20Shop/Furniture%20(6).png" className="w-100 " alt=""/>
            </Col>
            <Col lg={4}>
              <img src="https://bazaar.ui-lib.com/assets/images/Furniture%20Shop/Furniture%20(5).png" className="w-100" alt=""/>
@@ -60,7 +62,131 @@ export default function Furniture() {
      </section>
 
 
+     <section className="px-5 my-5">
+     <Container fluid>
+       <h4>Top New Product</h4>
+       <p>Tall blind but were, been folks not the expand</p>
+       <Row>
+         <Col lg={4}>
+          <Card>
+          <Card.Img variant="top" src="https://bazaar.ui-lib.com/_next/image?url=%2Fassets%2Fimages%2FFurniture%20Shop%2FFurniture%20(3).png&w=640&q=75" />
+          <Card.Body>
+            <Card.Title>Comfortable Sofa</Card.Title>
+            <span>$190.00</span>
+            <span>$200.00</span>
+            <Button variant="primary">+</Button>
+          </Card.Body>
+         </Card>
+         </Col>
+         <Col lg={4}>
+          <Card>
+          <Card.Img variant="top" src="https://bazaar.ui-lib.com/_next/image?url=%2Fassets%2Fimages%2FFurniture%20Shop%2FFurniture%20(3).png&w=640&q=75" />
+          <Card.Body>
+            <Card.Title>Comfortable Sofa</Card.Title>
+            <span>$190.00</span>
+            <span>$200.00</span>
+            <Button variant="primary">+</Button>
+          </Card.Body>
+         </Card>
+         </Col>
+         <Col lg={4}>
+          <Card>
+          <Card.Img variant="top" src="https://bazaar.ui-lib.com/_next/image?url=%2Fassets%2Fimages%2FFurniture%20Shop%2FFurniture%20(3).png&w=640&q=75" />
+          <Card.Body>
+            <Card.Title>Comfortable Sofa</Card.Title>
+            <span>$190.00</span>
+            <span>$200.00</span>
+            <Button variant="primary">+</Button>
+          </Card.Body>
+         </Card>
+         </Col>
+       </Row>
+     </Container>
+     </section>
      
+     <section className="px-5 my-5">
+     <Container fluid>
+       <h4>Top New Product</h4>
+       <p>Tall blind but were, been folks not the expand</p>
+       <Row>
+         <Col lg={4}>
+          <Card>
+          <Card.Img variant="top" src="https://bazaar.ui-lib.com/_next/image?url=%2Fassets%2Fimages%2FFurniture%20Shop%2FFurniture%20(3).png&w=640&q=75" />
+          <Card.Body>
+            <Card.Title>Comfortable Sofa</Card.Title>
+            <span>$190.00</span>
+            <span>$200.00</span>
+            <Button variant="primary">+</Button>
+          </Card.Body>
+         </Card>
+         </Col>
+         <Col lg={4}>
+          <Card>
+          <Card.Img variant="top" src="https://bazaar.ui-lib.com/_next/image?url=%2Fassets%2Fimages%2FFurniture%20Shop%2FFurniture%20(3).png&w=640&q=75" />
+          <Card.Body>
+            <Card.Title>Comfortable Sofa</Card.Title>
+            <span>$190.00</span>
+            <span>$200.00</span>
+            <Button variant="primary">+</Button>
+          </Card.Body>
+         </Card>
+         </Col>
+         <Col lg={4}>
+          <Card>
+          <Card.Img variant="top" src="https://bazaar.ui-lib.com/_next/image?url=%2Fassets%2Fimages%2FFurniture%20Shop%2FFurniture%20(3).png&w=640&q=75" />
+          <Card.Body>
+            <Card.Title>Comfortable Sofa</Card.Title>
+            <span>$190.00</span>
+            <span>$200.00</span>
+            <Button variant="primary">+</Button>
+          </Card.Body>
+         </Card>
+         </Col>
+       </Row>
+     </Container>
+     </section>
+     
+     <section className="px-5 my-5">
+     <Container fluid>
+       <h4>Top New Product</h4>
+       <p>Tall blind but were, been folks not the expand</p>
+       <Row>
+         <Col lg={4}>
+          <Card>
+          <Card.Img variant="top" src="https://bazaar.ui-lib.com/_next/image?url=%2Fassets%2Fimages%2FFurniture%20Shop%2FFurniture%20(3).png&w=640&q=75" />
+          <Card.Body>
+            <Card.Title>Comfortable Sofa</Card.Title>
+            <span>$190.00</span>
+            <span>$200.00</span>
+            <Button variant="primary">+</Button>
+          </Card.Body>
+         </Card>
+         </Col>
+         <Col lg={4}>
+          <Card>
+          <Card.Img variant="top" src="https://bazaar.ui-lib.com/_next/image?url=%2Fassets%2Fimages%2FFurniture%20Shop%2FFurniture%20(3).png&w=640&q=75" />
+          <Card.Body>
+            <Card.Title>Comfortable Sofa</Card.Title>
+            <span>$190.00</span>
+            <span>$200.00</span>
+            <Button variant="primary">+</Button>
+          </Card.Body>
+         </Card>
+         </Col>
+         <Col lg={4}>
+          <Card>
+          <Card.Img variant="top" src="https://bazaar.ui-lib.com/_next/image?url=%2Fassets%2Fimages%2FFurniture%20Shop%2FFurniture%20(3).png&w=640&q=75" />
+          <Card.Body>
+            <Card.Title>Comfortable Sofa</Card.Title>
+            <span>$190.00</span>
+            <span>$200.00</span>
+            <Button variant="primary">+</Button>
+          </Card.Body>
+         </Card>
+         </Col>
+       </Row>
+     </Container>
+     </section>
      
    </>
   );
